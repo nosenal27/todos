1b80bd00b7882b720fc4b19bcd7c42fdf2c36735
BEOMKYU CHUNG <nosenal27@gmail.com>
2019-01-16 13:40:53 +0900

    changed body.js to todo-list.js in attempt to fix bug

--- client/main.js
+++ client/main.js
@@ -1,6 +1,6 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-import '../imports/body.js';
+import '../imports/todo-list.js';
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
@@ -20,4 +20,3 @@ Template.hello.events({
     instance.counter.set(instance.counter.get() + 1);
   },
 });
-
